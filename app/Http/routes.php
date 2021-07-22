d7fddb66a88780cfc0119a157e7aa0010c414ba8
Shiro <zeddoktor@gmail.com>
2021-07-22 20:50:43 +0300

    create new task

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,8 @@
 |
 */
 
+use Illuminate\Http\Request;
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -18,8 +20,26 @@ Route::get('/', function () {
 //get all tasks
 Route::get('/tasks', function(){
     return view('tasks.index');
-});
+})->name('tasks.index');
 
 Route::get('/tasks/create', function(){
+    return view('tasks.create');
+})->name('tasks.create');
 
-});
+//post store task
+Route::post('tasks', function(Request $request){
+    $validator = Validator::make($request->all(), [
+        'name' => 'required|max:255',
+    ]);
+
+    if ($validator->fails()) {
+        return redirect(route('tasks.create'))
+            ->withInput()
+            ->withErrors($validator);
+    }
+    $task = new \App\Models\Task();
+    $task->name = $request->name;
+    $task->save();
+
+    return redirect(route('tasks.index'));
+})->name('tasks.store');
